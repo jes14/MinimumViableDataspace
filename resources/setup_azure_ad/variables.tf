120e5e54b12975d7225e610b625fc3030119bbe5
YJ <23358657+jes14@users.noreply.github.com>
2023-01-02 17:41:43 +0100

    Update variables.tf

--- resources/setup_azure_ad/variables.tf
+++ resources/setup_azure_ad/variables.tf
@@ -1,6 +1,6 @@
 # supply the tenant ID for your Azure Tenant here
 variable "tenant_id" {
-  default = "<YOUR_TENANT_ID>"
+  default = "970847bc-2b09-4574-a628-be2e84bd0c9d"
 }
 # App registration name for the Federated Credential for GH Actions
 variable "gh_actions_appname" {
@@ -20,7 +20,7 @@ variable "application_fc_pr_name" {
 }
 # name of your fork of MVD
 variable "github_repo" {
-  default = "<YOUR_FORK>/MinimumViableDataspace"
+  default = "jes14/MinimumViableDataspace"
 }
 # name of the storage account that'll hold the Terraform State for MVD deployments
 variable "tf_state_storageaccount" {
